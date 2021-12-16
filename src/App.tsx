363648f87dfbfcde5d1e9de40871d1ce2283b377
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2021-12-16 16:16:02 -0300

    Updt Cmmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { IndexHomePage } from './components/IndexHomePage';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
+    <>
+    <GlobalStyle />
+    <IndexHomePage />
     <div className="App">
-      <h1>Hello World!</h1>
     </div>
+    </>
   );
 }
 
---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    box-sizing: border-box;
+    outline: 0;
+    font-size: 62.5%;
+}
+:root {
+    
+}
+
+`;
+
+export default GlobalStyle;
