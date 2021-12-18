294194e642e020de3965b474c107d7097c1aaeb8
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2021-12-18 11:59:40 -0300

    updt cmmt

---
+++ src/components/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HomePage = styled.div`
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    display: flex;
+`;
+
+export const HomePageContainer = styled.div`
+    width: 800px;
+    height: 600px;
+`;
